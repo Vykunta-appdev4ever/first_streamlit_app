ebbc55b84fa7b9f38ad0621edca84ad157fcba0f
Vykunta-appdev4ever <136644330+Vykunta-appdev4ever@users.noreply.github.com>
2023-06-17 05:44:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ except URLError as e:
 #requirements.txt
 # import snowflake.connector
 streamlit.header("The fruit load list contains:")
-det get_fruit_load_list():
+def get_fruit_load_list():
       with my_cnx.cursor as my_cur:
            my_cur.execute("SELECT * from fruit_load_list") 
            return my_cur.fetchall()
